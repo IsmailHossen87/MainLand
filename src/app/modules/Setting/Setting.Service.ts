8bf8d97ad861cfd93eda55c839a33e5c5d8396b9
Ismail Hossen <ismailhosen8757@gmail.com>
2025-11-25 17:02:07 +0600

    TicketHistory

--- src/app/modules/Setting/Setting.Service.ts
+++ src/app/modules/Setting/Setting.Service.ts
@@ -2,28 +2,33 @@ import { ISettings, Settings } from "./Setting.model";
 
 
 // Create or update single settings document
-const upsertSettings = async (data: ISettings) => {
-  const result = await Settings.findOneAndUpdate({}, data, {
-    upsert: true,
-    new: true
-  });
+const updateSetting = async (data: { type: string; title: string; content: string }) => {
+  const { type, title, content } = data;
+
+  const result = await Settings.findOneAndUpdate(
+    { type },
+    { title, content },
+    { upsert: true, new: true }
+  );
+
   return result;
 };
 
-
-// Get all settings (single document)
-const getSettings = async () => {
-  const result = await Settings.findOne();
-  return result;
-};
-
-const getSpecificSetting = async (key: string) => {
-  const settingsDoc = await Settings.findOne(); 
-  if (!settingsDoc) return null;
-  const value = (settingsDoc as any)[key];
-  return value ? { [key]: value } : null;
-};
 export const SettingService = {
-  upsertSettings,
-  getSettings ,getSpecificSetting
+  updateSetting,
 };
+
+
+
+// // Get all settings (single document)
+// const getSettings = async () => {
+//   const result = await Settings.findOne();
+//   return result;
+// };
+
+// const getSpecificSetting = async (key: string) => {
+//   const settingsDoc = await Settings.findOne(); 
+//   if (!settingsDoc) return null;
+//   const value = (settingsDoc as any)[key];
+//   return value ? { [key]: value } : null;
+// };
