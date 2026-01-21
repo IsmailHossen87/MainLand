d1d337e80eb305bce7b00d0fc614a151eb13334c
Ismail Hossen <ismailhosen8757@gmail.com>
2026-01-21 11:03:21 +0600

    firebaseJson

--- dist/helpers/firebase.js
+++ dist/helpers/firebase.js
@@ -4,11 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.firebaseAdmin = void 0;
+const fs_1 = __importDefault(require("fs"));
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const firebaseServiceAccount_json_1 = __importDefault(require("../config/firebaseServiceAccount.json"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
+if (!serviceAccountPath) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT_PATH is not defined in .env");
+}
+// JSON file read & parse
+const serviceAccount = JSON.parse(fs_1.default.readFileSync(serviceAccountPath, "utf-8"));
 if (!firebase_admin_1.default.apps.length) {
     firebase_admin_1.default.initializeApp({
-        credential: firebase_admin_1.default.credential.cert(firebaseServiceAccount_json_1.default),
+        credential: firebase_admin_1.default.credential.cert(serviceAccount),
     });
 }
 exports.firebaseAdmin = firebase_admin_1.default;
--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -1,9 +1,21 @@
+import fs from "fs";
 import admin from "firebase-admin";
-import serviceAccount from "../config/firebaseServiceAccount.json";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
+
+if (!serviceAccountPath) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT_PATH is not defined in .env");
+}
+
+// JSON file read & parse
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+        credential: admin.credential.cert(serviceAccount),
     });
 }
 
