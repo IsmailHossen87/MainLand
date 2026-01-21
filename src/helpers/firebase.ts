4a663f28934f6ab404c931a4026c6858817e5d2c
Ismail Hossen <ismailhosen8757@gmail.com>
2026-01-21 16:55:41 +0600

    firebase.json

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -9,7 +9,7 @@ const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
 if (!serviceAccountPath) {
     throw new Error("FIREBASE_SERVICE_ACCOUNT_PATH is not defined in .env");
 }
-
+// shakalaka
 // JSON file read & parse
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
 
